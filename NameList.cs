94e0756b5b38cdf3f2aaed9211e15f8cfb12d82a
Viktor Thörn <Viktor.e.thorn@hotmail.com>
2024-09-30 12:10:41 +0200

    Moved the code to a separate NameList class to clean up the code.  Removed the separate codeblocks and put them into functions to increase readability. Made functions to instance list with names, add a name, print list, sort list, and search for name.

---
+++ NameList.cs
@@ -0,0 +1,59 @@
+﻿using code_revision_assignment;
+using System;
+using System.Collections.Generic;
+
+namespace code_revision_assignment
+{
+    internal class NameList
+    {
+        static List<string> names = new List<string>();
+        
+
+        internal static void CreateList() //created a function to add names into the list, in order to have a few names sto start of the list before we can manage it.
+        {
+            names.Add("Anna");
+            names.Add("John");
+            names.Add("Alice");
+            names.Add("Boris");
+        }
+        
+        internal static void AddName() //added method in order to add more names to the list
+        {
+            Console.WriteLine("Please enter the name you would like to add");
+            names.Add(Console.ReadLine());
+
+        }
+
+        internal static void PrintList() //added a printfunction to the list to make decrease duplicates in code
+        {
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        internal static void SortList() //made a sortfunction
+        {
+            names.Sort(); //Sor the names alphabetically
+            Console.WriteLine("\nSorted lists:");
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        internal static void SearchName() //made a function to search for name
+        {
+            Console.WriteLine("\nEnter name to search:");
+            string searchName = Console.ReadLine();
+            if (names.Contains(searchName))
+            {
+                Console.WriteLine($"{searchName} is in the list.");
+            }
+            else
+            {
+                Console.WriteLine($"{searchName} is not in the list.");
+            }
+        }
+    }
+}
